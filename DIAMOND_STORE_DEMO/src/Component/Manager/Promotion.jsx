c6838fd593d604154674990d8b49eb2a7614764e
cbq114 <nguyentruongtin5@gmail.com>
2024-07-06 20:58:18 +0700

    fix

--- DIAMOND_STORE_DEMO/src/Component/Manager/Promotion.jsx
+++ DIAMOND_STORE_DEMO/src/Component/Manager/Promotion.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useAuth } from '../Authentication/AuthContext';
+import { useAuth } from '../../Authentication/AuthContext';
 import {
     Table,
     TableBody,
