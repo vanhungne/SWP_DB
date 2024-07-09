7f0478482370e7fe18ccdb8d7f41b745378069d7
Hưng <138867887+vanhungne@users.noreply.github.com>
2024-07-09 13:31:13 +0700

    Update config.jsx

--- DIAMOND_STORE_DEMO/src/Config/config.jsx
+++ DIAMOND_STORE_DEMO/src/Config/config.jsx
@@ -1,4 +1,4 @@
 
-//export const API_URL = 'https://diamondstore.wittyglacier-46436ee8.japaneast.azurecontainerapps.io/';
-export const API_URL = 'http://localhost:8080/';
+export const API_URL = 'https://diamondstore.wittyglacier-46436ee8.japaneast.azurecontainerapps.io/';
+//export const API_URL = 'http://localhost:8080/';
 
